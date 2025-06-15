3b9015d70087be5ea4d76a530afc11e6c8ae5f3e
Kirill Barkov <freedomdragon777@gmail.com>
2025-06-15 17:09:16 +0400

    Обновлен компонент HomePage: добавлены стиль и текстовые изменения, обновлен SCSS. Увеличена толщина границы в Header. Добавлены лого.

--- src/pages/HomePage/HomePage.tsx
+++ src/pages/HomePage/HomePage.tsx
@@ -8,7 +8,26 @@ interface HomePageProps {
 const HomePage: FC<HomePageProps> = () => {
   return (
     <div className={cls.homePage}>
-      <h1>Руководство по модификациям</h1>
+      <h1 className={cls.title}>
+        The Elder Scrolls V: Skyrim <br />
+        Anniversary Edition
+      </h1>
+      <p className={cls.description}>Ещё одно руководство по моддингу Скайрима.</p>
+
+      <div className={cls.buttonWrapper}>
+        <button className={cls.gradientButton}>
+          <span>Начать</span>
+          <svg
+            className={cls.buttonIcon}
+            width="20"
+            height="20"
+            viewBox="0 0 20 20"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path d="M8.22 5.22a.75.75 0 0 1 1.06 0l4.25 4.25a.75.75 0 0 1 0 1.06l-4.25 4.25a.75.75 0 0 1-1.06-1.06L11.94 10 8.22 6.28a.75.75 0 0 1 0-1.06Z" />
+          </svg>
+        </button>
+      </div>
     </div>
   );
 };
