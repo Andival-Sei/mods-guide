f34df4daa7e538a68d6d5c38e05da91f351ccf97
Kirill Barkov <freedomdragon777@gmail.com>
2025-06-15 21:52:24 +0400

    Обновлены стили и логика MainLayout: убрано выделение фона для главной странице.

--- src/components/MainLayout/MainLayout.tsx
+++ src/components/MainLayout/MainLayout.tsx
@@ -1,14 +1,17 @@
 import cls from './MainLayout.module.scss';
 import { Suspense } from 'react';
-import { Outlet } from 'react-router-dom';
+import { Outlet, useLocation } from 'react-router-dom';
 import Header from '../Header/Header.tsx';
 
 const MainLayout = () => {
+  const location = useLocation();
+  const isHomePage = location.pathname === '/';
+
   return (
     <div className={cls.mainLayout}>
       <Header />
       <div className={cls.mainWrapper}>
-        <div className={cls.contentWrapper}>
+        <div className={`${cls.contentWrapper} ${isHomePage ? cls.homePageContent : ''}`}>
           <main>
             <Suspense>
               <Outlet />
