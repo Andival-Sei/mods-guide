eebed35dbcfa3fd446baf71b4ddca41bbd67b662
Kirill Barkov <freedomdragon777@gmail.com>
2025-06-17 22:21:14 +0400

    Обновлен компонент TextHighlight: добавлена поддержка подчеркивания через новый проп "underline". Дополнена CommonInstructionsPage: новый текстовый контент и обновленный стиль для подчеркивания.

--- src/components/TextHighlight/TextHighlight.tsx
+++ src/components/TextHighlight/TextHighlight.tsx
@@ -3,12 +3,17 @@ import cls from './TextHighlight.module.scss';
 
 interface TextHighlightProps {
   children: ReactNode;
+  underline?: boolean;
 }
 
 const TextHighlight = (props: TextHighlightProps) => {
-  const { children } = props;
+  const { children, underline } = props;
 
-  return <span className={cls.highlight}>{children}</span>;
+  return (
+    <span className={`${cls.highlight} ${underline ? cls.underline : ''}`}>
+      {children}
+    </span>
+  );
 };
 
 export default TextHighlight;
