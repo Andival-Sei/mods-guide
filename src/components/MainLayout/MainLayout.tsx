f7b1db39b6ed43c4eee15b121c749486cc92b9a5
Kirill Barkov <freedomdragon777@gmail.com>
2025-06-17 15:51:02 +0400

    Добавлена логика автоматической прокрутки к началу страницы при изменении маршрута.

--- src/components/MainLayout/MainLayout.tsx
+++ src/components/MainLayout/MainLayout.tsx
@@ -1,16 +1,23 @@
 import cls from './MainLayout.module.scss';
-import { Suspense } from 'react';
+import { Suspense, useEffect, useRef } from 'react';
 import { Outlet, useLocation } from 'react-router-dom';
 import Header from '../Header/Header.tsx';
 
 const MainLayout = () => {
   const location = useLocation();
   const isHomePage = location.pathname === '/';
+  const mainWrapperRef = useRef<HTMLDivElement>(null);
+
+  useEffect(() => {
+    if (mainWrapperRef.current) {
+      mainWrapperRef.current.scrollTop = 0;
+    }
+  }, [location.pathname]);
 
   return (
     <div className={cls.mainLayout}>
       <Header />
-      <div className={cls.mainWrapper}>
+      <div ref={mainWrapperRef} className={cls.mainWrapper}>
         <div className={`${cls.contentWrapper} ${isHomePage ? cls.homePageContent : ''}`}>
           <main>
             <Suspense>
